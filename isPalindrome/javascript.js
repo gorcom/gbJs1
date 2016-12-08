1a64172922bf98d2477a9e0c80c59e5a0fcb2d3e
gorcom <gorcom77@gmail.com>
2016-12-08 16:28:16 +0300

    Добавил 2-й вариант

--- isPalindrome/javascript.js
+++ isPalindrome/javascript.js
@@ -1,3 +1,10 @@
 function isPalindrome(string) {
     return string.split(/[^a-zA-Zа-яА-Я0-9]/).join('').toLowerCase() == string.split(/[^a-zA-Zа-яА-Я0-9]/).join('').split('').reverse().join('').toLowerCase();
 }
+
+
+//version 2
+
+function isPalindrome(string) {
+    return string.replace(/[^a-zA-Zа-яА-Я0-9]/g,'').toLowerCase() == string.replace(/[^a-zA-Zа-яА-Я0-9]/g,'').split('').reverse().join('').toLowerCase();
+}
